f17b5fe8191676c378c74e710ccc4b4e8935b78f
Luca Lambia <luca@lambia.it>
2026-01-13 10:21:55 +0100

    minor

--- readme.md
+++ readme.md
@@ -1,4 +1,22 @@
-gitignore
-npm init
-npm i express
-comandi script
+# Creazione progetto
+- Inizializzare una repo con il gitignore per escludere node_modules e package.json
+- Inizializzare un progetto con npm init
+- Installare express con `npm i express`
+- Aggiungere al package.json gli script per npm run:
+  ```
+  "scripts": {
+    "start": "node server.js",
+    "dev": "node --watch server.js",
+  },
+  ```
+
+# Configurazione express
+- Partiamo dall'esempio in documentazione Express.
+- Aggiungiamo le varie rotte da gestire.
+- Eventualmente aggiungiamo il middleware per file statici
+
+# Avvio progetti
+- Se si clona una repo: `npm install` o `npm i` per creare il package-lock e la cartella node_modules.
+Utile anche se abbiamo "fatto casino" e vogliamo eliminare e ricreare tutto.
+- Avviare il progetto con il comando specificato in package.json.
+Di solito è `npm run dev` o `npm run watch`
--- server.js
+++ server.js
@@ -2,9 +2,51 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// in produzione è consigliabile bypassare con reverse proxy nginx configurato per servire direttamente i file statici
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.type("html").send('<h1>Benvenuto nel server della mia pizzeria</h1>');
+});
+
+app.get("/menu", (req, res) => {
+
+	const menu = [
+		{
+			name: "Margherita",
+			image: "imgs/pizze/margherita.webp",
+			ingredients: ["pomodoro", "mozzarella"],
+		}, {
+			name: "Marinara",
+			image: "imgs/pizze/marinara.jpeg",
+			ingredients: ["pomodoro", "aglio", "origano"],
+		}, {
+			name: "Diavola",
+			image: "imgs/pizze/diavola.jpeg",
+			ingredients: ["pomodoro", "mozzarella", "salame piccante"],
+		}, {
+			name: "Bufalina",
+			image: "imgs/pizze/bufalina.jpeg",
+			ingredients: ["pomodoro", "mozzarella di bufala"],
+		}, {
+			name: "4 formaggi",
+			image: "imgs/pizze/4_formaggi.jpeg",
+			ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
+		}
+	];
+
+	res.json(menu);
+
 })
+
+app.get("/json", (req, res) => {
+	const docente = {
+		nome: "Luca",
+		cognome: "Lambiase"
+	};
+
+	res.type("json").send(docente);
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
