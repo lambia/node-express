c54feae4207d56216b258b5cca93fa57500a2d46
Luca Lambia <luca@lambia.it>
2026-01-13 10:39:48 +0100

    minor

--- server.js
+++ server.js
@@ -2,7 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const menu = [
+	{
+		name: "Margherita",
+		image: "imgs/pizze/margherita.webp",
+		ingredients: ["pomodoro", "mozzarella"],
+	}, {
+		name: "Marinara",
+		image: "imgs/pizze/marinara.jpeg",
+		ingredients: ["pomodoro", "aglio", "origano"],
+	}, {
+		name: "Diavola",
+		image: "imgs/pizze/diavola.jpeg",
+		ingredients: ["pomodoro", "mozzarella", "salame piccante"],
+	}, {
+		name: "Bufalina",
+		image: "imgs/pizze/bufalina.jpeg",
+		ingredients: ["pomodoro", "mozzarella di bufala"],
+	}, {
+		name: "4 formaggi",
+		image: "imgs/pizze/4_formaggi.jpeg",
+		ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
+	}
+];
+
 // in produzione è consigliabile bypassare con reverse proxy nginx configurato per servire direttamente i file statici
+// oppure si può aggiungere un layer di caching sempre con nginx o con varnish
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
@@ -10,42 +35,29 @@ app.get('/', (req, res) => {
 });
 
 app.get("/menu", (req, res) => {
-
-	const menu = [
-		{
-			name: "Margherita",
-			image: "imgs/pizze/margherita.webp",
-			ingredients: ["pomodoro", "mozzarella"],
-		}, {
-			name: "Marinara",
-			image: "imgs/pizze/marinara.jpeg",
-			ingredients: ["pomodoro", "aglio", "origano"],
-		}, {
-			name: "Diavola",
-			image: "imgs/pizze/diavola.jpeg",
-			ingredients: ["pomodoro", "mozzarella", "salame piccante"],
-		}, {
-			name: "Bufalina",
-			image: "imgs/pizze/bufalina.jpeg",
-			ingredients: ["pomodoro", "mozzarella di bufala"],
-		}, {
-			name: "4 formaggi",
-			image: "imgs/pizze/4_formaggi.jpeg",
-			ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
-		}
-	];
-
+	// res.type("json").send(menu);
+	// o più semplicemente: 
 	res.json(menu);
-
 })
 
-app.get("/json", (req, res) => {
-	const docente = {
-		nome: "Luca",
-		cognome: "Lambiase"
+app.get("/debug", (req, res) => {
+	const richiestaSemplificata = {
+		query: req.query, //query params come ?chiave=valore
+		params: req.params, //parametri di rotta (non ancora visti)
+		body: req.body, //body richesta json/form (non ancora visti)
+		headers: req.headers, //header allegati alla richiesta
+		method: req.method, //metodo HTTP (GET, POST, …)
+		originalUrl: req.originalUrl, //URL richiesto
+		path: req.path, //path (senza query)
+		protocol: req.protocol, //http / https
+		ip: req.ip, //IP client
+		secure: req.secure, //true se HTTPS
+		xhr: req.xhr, //true se AJAX
 	};
 
-	res.type("json").send(docente);
+	console.log("Ricevuta richiesta: ", richiestaSemplificata);
+
+	res.json(richiestaSemplificata);
 });
 
 app.listen(port, () => {
