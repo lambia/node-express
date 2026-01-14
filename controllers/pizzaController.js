77202502e36c8b4cbeae6d9000108d42565932ab
Luca Lambia <luca@lambia.it>
2026-01-14 10:18:36 +0100

    spostata logica in controller

---
+++ controllers/pizzaController.js
@@ -0,0 +1,68 @@
+const pizzas = require("../data/pizzas");
+
+function index(req, res) {
+
+	let results = pizzas;
+
+	if (req.query.ingredients) {
+
+		const needle = req.query.ingredients;
+		results = pizzas.filter(pizza => pizza.ingredients.includes(needle));
+
+	}
+
+	res.json(results);
+}
+
+function show(req, res) {
+	const id = parseInt(req.params.id);
+	const result = pizzas.find(pizza => pizza.id === id);
+
+	if (!result) {
+
+		res.status(404);
+
+		res.json({
+			error: "Not found",
+			message: "Pizza non trovata"
+		});
+	}
+
+	res.json(result);
+}
+
+function store(req, res) {
+	// copiamo la logica della store
+}
+
+function update(req, res) {
+	// copiamo la logica dell'update
+}
+
+function modify(req, res) {
+	// copiamo la logica dell'update
+}
+
+function destroy(req, res) {
+	const id = parseInt(req.params.id);
+	const result = pizzas.find(pizza => pizza.id === id);
+
+	if (!result) {
+
+		res.status(404);
+
+		res.json({
+			error: "Not found",
+			message: "Pizza non trovata"
+		});
+	}
+
+	const pizzaIndex = pizzas.indexOf(result);
+
+	pizzas.splice(pizzaIndex, 1);
+
+	res.sendStatus(204);
+}
+
+// esportiamo tutto
+module.exports = { index, show, store, update, modify, destroy }
--- routers/pizzas.js
+++ routers/pizzas.js
@@ -1,81 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const pizzas = require("../data/pizzas");
+const pizzaController = require("../controllers/pizzaController");
 
-//index
-router.get("/", (req, res) => {
-
-	let results = pizzas;
-
-	if (req.query.ingredients) {
-
-		const needle = req.query.ingredients;
-		results = pizzas.filter(pizza => pizza.ingredients.includes(needle));
-
-	}
-
-	res.json(results);
-
-});
-
-// show
-router.get("/:id", (req, res) => {
-
-	const id = parseInt(req.params.id);
-	const result = pizzas.find(pizza => pizza.id === id);
-
-	if (!result) {
-
-		res.status(404);
-
-		res.json({
-			error: "Not found",
-			message: "Pizza non trovata"
-		});
-	}
-
-	res.json(result);
-
-});
-
-// store
-router.post('/', function (req, res) {
-	res.send('Creazione nuova pizza');
-});
-
-// update
-router.put('/:id', function (req, res) {
-	res.send('Modifica integrale della pizza ' + req.params.id);
-});
-
-// modify
-router.patch('/:id', function (req, res) {
-	res.send('Modifica parziale della pizza ' + req.params.id);
-});
-
-// destroy
-router.delete('/:id', function (req, res) {
-
-	const id = parseInt(req.params.id);
-	const result = pizzas.find(pizza => pizza.id === id);
-
-	if (!result) {
-
-		res.status(404);
-
-		res.json({
-			error: "Not found",
-			message: "Pizza non trovata"
-		});
-	}
-
-	const pizzaIndex = pizzas.indexOf(result);
-
-	pizzas.splice(pizzaIndex, 1);
-
-	res.sendStatus(204);
-
-});
+router.get("/", pizzaController.index);
+router.get("/:id", pizzaController.show);
+router.post('/', pizzaController.store);
+router.put('/:id', pizzaController.update);
+router.patch('/:id', pizzaController.modify);
+router.delete('/:id', pizzaController.destroy);
 
 module.exports = router;
