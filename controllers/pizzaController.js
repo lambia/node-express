c81559c6ec2ece40f9f8ac7c7363a3cd5008af8c
Luca Lambia <luca@lambia.it>
2026-01-15 09:33:11 +0100

    add store and update methods

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -30,11 +30,41 @@ function show(req, res) {
 }
 
 function store(req, res) {
-	// copiamo la logica della store
+	const pizzasIdsList = pizzas.map(pizza => pizza.id);
+	const lastId = Math.max(...pizzasIdsList);
+	const newId = lastId + 1;
+
+	const nuovaPizza = {
+		id: newId,
+		name: req.body.name,
+		image: req.body.image,
+		ingredients: req.body.ingredients
+	};
+
+	pizzas.push(nuovaPizza);
+
+	res.status(201);
+	res.json(pizzas[pizzas.length - 1]);
 }
 
 function update(req, res) {
-	// copiamo la logica dell'update
+
+	const id = parseInt(req.params.id);
+	const result = pizzas.find(pizza => pizza.id === id);
+
+	if (!result) {
+		//Il return serve ad interrompere davvero la funzione
+		return res.status(404).json({
+			error: "Not found",
+			message: "Pizza non trovata"
+		});
+	}
+
+	result.name = req.body.name;
+	result.image = req.body.image;
+	result.ingredients = req.body.ingredients;
+
+	res.json(result);
 }
 
 function modify(req, res) {
